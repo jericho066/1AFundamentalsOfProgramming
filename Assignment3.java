168aaeef0943bb1eff9f2247277a40e0bdae4877
Jerico Oliver <oliverjerico96@gmail.com>
2026-09-01 18:29:22 +0800

    Add Assignment3

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3 { // Declaring a public class named Assignment3
+    public static void main(String[] args) { // Declaring the main method
+
+        String firstname = "Hero Jerico"; // Declaring String variable for the first name
+        String lastname = "Oliver"; // Declaring String variable for the last name
+        String address = "Urdaneta City"; // Declaring String variable for the address
+        String mobileNumber = "09100077884"; // Declaring String variable for the mobile number
+        int age = 20; // Declaring an int variable for the age
+        double gwa = 95.50; // Declaring double variable for the GWA
+
+        System.out.println("Firstname: " + firstname); // Displays the first name.
+        System.out.println("Lastname: " + lastname); // Displays the last name.
+        System.out.println("Address: " + address); // Displays the address.
+        System.out.println("Mobile Number: " + mobileNumber); // Displays the mobile number.
+        System.out.println("Age: " + age); // Displays the age.
+        System.out.println("GWA: " + gwa); // Displays the GWA.
+    } // Closing the main method
+} // Closing the Assignment3 class
